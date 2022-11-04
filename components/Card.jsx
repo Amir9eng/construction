6af3d8e98e6fcefbff3739c66254f69299826fc0
Amir9eng <amirmukhtarme@gmail.com>
2022-11-04 20:06:51 +0100

    responsiveness

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,7 +6,7 @@ const Card = props => {
       <Image alt='card' width={293} height={182} src={props.image} />
       <aside className='border-2 border-gray-400 mt-4 px-4 py-8'>
         <p className='text-gray-900 font-bold my-4'>{props.service}</p>
-        <p className='text-gray-500'>{props.paragraph}</p>
+        <p className='text-gray-600'>{props.paragraph}</p>
         <button className='w-[8.625rem] h-[1.75rem] border-2 border-gray-400 mt-6 font-semibold text-gray-500'>
           LEARN MORE
         </button>
